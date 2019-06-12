40ef2ffe4cf3949134f66ccd31b2cd16ad91089f
Téo le Dozo <teo@couaque.fr>
2019-06-12 21:55:25 +0200

    Correcting a bug where the ips file was not cleared before putting the IPs, causing them to be appended instead.

--- processing.sh
+++ processing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat ns.log* | cut -d ":" -f 8 | grep "Source" | sed 's/Source//g' | sort | uniq -c | sort -r -s -n -k 1,1 >> ips
+cat ns.log* | cut -d ":" -f 8 | grep "Source" | sed 's/Source//g' | sort | uniq -c | sort -r -s -n -k 1,1 > ips
